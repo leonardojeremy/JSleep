d57ae67ecd318180d22ab2042e3f6ad29d335227
Leonardo Jeremy Pongpare Munda <leonardo.munda141@gmail.com>
2022-10-30 16:41:34 +0700

    TP Modul 6

--- src/LeonardoJeremyJSleepDN/JSleep.java
+++ src/LeonardoJeremyJSleepDN/JSleep.java
@@ -1,22 +1,34 @@
 package LeonardoJeremyJSleepDN;
-//import java.sql.Date;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 public class JSleep
 {
-    public static void main(String[] args){
-        ArrayList<Room> RoomSerialized = new ArrayList<Room>();
-        for(int i = 0; i < 5; i++){
-            RoomSerialized.add(i, JSleep.createRoom());
-            System.out.println(RoomSerialized.get(i).toString());
-        }
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
     }
 
-    public static Room createRoom(){
-        Price price = new Price (100000, 0.5);
-        Room room = new Room("Hotel", 30, price, Facility.AC, City.DEPOK, "Jalan Margonda Raya");
-        return room;
+    public static void main(String[] args) throws IOException
+    {
+        String filepath = "C:\\_UNI\\OOP\\_Praktikum OOP\\UNTUK PRAKTIKUM OOP\\JSleep\\src\\city.json";
+        Gson gson = new Gson();
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states: ");
+            input.listOfStates.forEach(state->System.out.println(state));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
     
 
